226111af42c44afcca0f8c39105718f010fb81b4
Daniel Bengtsson <daniel@bengtssons.info>
2010-10-08 21:38:44 +0200

    Send mail also when a site goes back up

--- alive.py
+++ alive.py
@@ -12,8 +12,8 @@ import smtplib
 from email.mime.text import MIMEText
 
 PARSER = OptionParser(usage="%prog filename", description="""
-This script takes as input a URL and checks with wget if
-it can be accessed.
+This script takes as input one or several URLs and checks with wget if
+they can be accessed.
 """)
 
 PARSER.add_option("-u", "--url", action="store", type="string", dest="URL", help="URL to try to retrieve (Required) You can write several URLs separated by space, but remember to quote the string.")
@@ -38,6 +38,26 @@ def write( text ):
         print text,
         sys.stdout.flush()
 
+def send_mail(subject, body):
+    """Send a mail using smtp server on localhost"""
+    write( "Mailing...")
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    if OPTIONS.FROM:
+        msg['From'] = OPTIONS.FROM
+    msg['To'] = OPTIONS.TO
+    smtp = smtplib.SMTP()
+    if OPTIONS.VERBOSE:
+        smtp.set_debuglevel(True)
+    try:
+        smtp.connect()
+    except:
+        print "Could not send email, do you have an SMTP server running on localhost?"
+        return False
+    smtp.sendmail(OPTIONS.FROM, [OPTIONS.TO], msg.as_string())
+    smtp.quit()
+    return True
+
 for URL in URLS:
 
     if not CONFIG.has_section( URL ):
@@ -61,27 +81,18 @@ for URL in URLS:
             write( "State already known" )
 
         if not PREV_STATUS and OPTIONS.TO:
-            write( "Mailing...")
-            MSG = MIMEText("Site is down at %s" % datetime.datetime.now().ctime() )
-            MSG['Subject'] = "%s Down" % URL
-            if OPTIONS.FROM:
-                MSG['From'] = OPTIONS.FROM
-            MSG['To'] = OPTIONS.TO
-            S = smtplib.SMTP()
-            if OPTIONS.VERBOSE:
-                S.set_debuglevel(True)
-            try:
-                S.connect()
-            except:
-                print "Could not send email, do you have an SMTP server running on localhost?"
+            if( not send_mail( "%s Down" % URL, "Site is down at %s" % datetime.datetime.now().ctime() ) ):
                 continue
-            S.sendmail(OPTIONS.FROM, [OPTIONS.TO], MSG.as_string())
-            S.quit()
 
         CONFIG.set( URL, "Down", "yes" )
 
     else:
         write( "Up\n" )
+
+        if PREV_STATUS and OPTIONS.TO:
+            if( not send_mail( "%s Up" % URL, "Site is up at %s" % datetime.datetime.now().ctime() ) ):
+                continue
+
         CONFIG.set( URL, "Down", "no" )
 
 # Write the configuration file
